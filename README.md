664bba4c306a2c342d40079dc6d9d6357704cf4b
salmanefrihmat <sam.sensation@hotmail.fr>
2021-08-09 15:10:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-svm2ft
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-svm2ft)
---
+++ index.ts
@@ -0,0 +1,32 @@
+import { TreeGrid, Filter, Page } from '@syncfusion/ej2-treegrid';
+import { sampleData } from './datasource.ts';
+TreeGrid.Inject(Page, Filter);
+
+let treeGridObj: TreeGrid = new TreeGrid({
+  dataSource: sampleData,
+  childMapping: 'subtasks',
+  treeColumnIndex: 1,
+  allowFiltering: true,
+  allowPaging: true,
+  pageSettings: { pageSize: 15 },
+  columns: [
+    { field: 'taskID', headerText: 'Task ID', width: 90, textAlign: 'Right' },
+    {
+      field: 'taskName',
+      headerText: 'Task Name',
+      width: 180,
+      textAlign: 'Left'
+    },
+    {
+      field: 'startDate',
+      headerText: 'Start Date',
+      width: 90,
+      textAlign: 'Right',
+      type: 'date',
+      format: 'yMd'
+    },
+    { field: 'duration', headerText: 'Duration', width: 80, textAlign: 'Right' }
+  ]
+});
+
+treeGridObj.appendTo('#TreeGrid');
